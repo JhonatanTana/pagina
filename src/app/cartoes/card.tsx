887ca0fd96b52cb78284bdccbe4248b276d71d99
JhonatanTana <jhonatan.tana@alunos.unis.edu.br>
2024-05-02 21:28:13 -0300

    Sugestao

--- src/app/cartoes/card.tsx
+++ src/app/cartoes/card.tsx
@@ -4,71 +4,77 @@ export default function Cartas() {
     return (
 
         <div className="p-4 sm:ml-64 justify-center items-center flex-1 bg-paleta-2.1">
-            <div className="p-10 border-gray-600 border rounded-lg dark:border-black flex-auto">
-                <div className="grid grid-cols-1 gap-4 mb-4 h-auto">
-                    <div className="flex items-center justify-center rounded dark:bg-paleta-2.2/60 transition duration-200 ease-in-out transform hover:opacity-100 hover:scale-105 hover:z-50">
-                        <h5 className="px-10 max-w-sm text-2xl left-0 fixed text-white dark:text-white">
-                            Aula 1
+            <div className="p-10 flex-auto">
+                <div className="grid grid-cols-1 gap-4 mb-4 h-auto" >
+                    <div className="shadow-lg bg-black flex items-center justify-center rounded dark:bg-paleta-2.2 transition duration-200 ease-in-out transform hover:bg-paleta-2.2.1 ">
+                        <h5 className="px-10 max-w-sm text-2xl left-0 fixed text-black dark:text-black select-none ">
+                            Aula #1
                         </h5>
-                        <div className='text-white/50 hover:text-white/90 p-4 max-w-sm items-center justify-center overflow-hidden text-s tracking-tight duration-1000 ease-in-out'>
+                        <div className='text-black p-4 max-w-xl items-center justify-center overflow-hidden text-s tracking-tight duration-1000 ease-in-out'>
                            lo Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad excepturi dolor consequuntur quae provident nihil minus error nemo, voluptatem velit deserunt sapiente mollitia officiis fuga facere illum quis! Non, tenetur!
                         </div>
-                        <Button variant='contained' className=' content-end items-end fixed right-36 flex bg-gradient-to-tr from-blue-800 from-10% to-custom-blue-5 text-white transition-all duration-900 hover:scale-150'>PDF</Button>
+                        <Button variant='contained' className=' content-end items-end fixed right-36 flex bg-paleta-2.3 text-white transition-transform duration-900 hover:scale-110 hover:bg-paleta-2.4'>PDF</Button>
+                        <Button variant='contained' className=' content-end items-end fixed right-8 flex bg-paleta-2.3 text-white transition-transform duration-900 hover:scale-110 hover:bg-paleta-2.4'>Assistir</Button>
                     </div>
-                    <div className="flex items-center justify-center rounded dark:bg-paleta-2.2 transition duration-200 ease-in-out transform hover:opacity-100 hover:scale-105 hover:z-50">
-                        <h5 className="px-10 max-w-sm text-2xl left-0 fixed text-white dark:text-white">
-                            Aula 2
+                    <div className="shadow-lg bg-black flex items-center justify-center rounded dark:bg-paleta-2.2 transition duration-200 ease-in-out transform hover:bg-paleta-2.2.1 hover:opacity-100 hover:z-50">
+                        <h5 className="px-10 max-w-sm text-2xl left-0 fixed text-black dark:text-black select-none">
+                            Aula #2
                         </h5>
-                        <div className='text-white/50 hover:text-white/90 p-2 max-w-sm items-center justify-center overflow-hidden text-s tracking-tight duration-1000 ease-in-out '>
+                        <div className='text-black p-2 max-w-xl items-center justify-center overflow-hidden text-s tracking-tight duration-1000 ease-in-out '>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad excepturi dolor consequuntur quae provident nihil minus error nemo, voluptatem velit deserunt sapiente mollitia officiis fuga facere illum quis! Non, tenetur!
                         </div>
-                        <Button variant='contained' className=' content-end items-end fixed right-36 flex bg-gradient-to-tr from-blue-800 from-10% to-custom-blue-5 text-white transition-all duration-900 hover:scale-150'>PDF</Button>
-
+                        <Button variant='contained' className=' content-end items-end fixed right-36 flex bg-paleta-2.3 text-white transition-transform duration-900 hover:scale-110 hover:bg-paleta-2.4'>PDF</Button>
+                        <Button variant='contained' className=' content-end items-end fixed right-8 flex bg-paleta-2.3 text-white transition-transform duration-900 hover:scale-110 hover:bg-paleta-2.4'>Assistir</Button>
                     </div>
-                    <div className="flex items-center justify-center rounded dark:bg-paleta-2.2 transition duration-200 ease-in-out transform hover:opacity-100 hover:scale-105 hover:z-50">
-                        <h5 className="px-10 max-w-sm text-2xl left-0 fixed text-white dark:text-white">
-                            Aula 3
+                    <div className="shadow-lg bg-black flex items-center justify-center rounded dark:bg-paleta-2.2 transition duration-200 ease-in-out transform hover:bg-paleta-2.2.1 hover:opacity-100 hover:z-50">
+                        <h5 className="px-10 max-w-sm text-2xl left-0 fixed text-black dark:text-black select-none">
+                            Aula #3
                         </h5>
-                        <div className='text-white/50 hover:text-white/90 p-2 max-w-sm items-center justify-center overflow-hidden text-s tracking-tight duration-1000 ease-in-out '>
+                        <div className='text-black p-2 max-w-xl items-center justify-center overflow-hidden text-s tracking-tight duration-1000 ease-in-out '>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad excepturi dolor consequuntur quae provident nihil minus error nemo, voluptatem velit deserunt sapiente mollitia officiis fuga facere illum quis! Non, tenetur!
                         </div>
-                        <Button variant='contained' className=' content-end items-end fixed right-36 flex bg-gradient-to-tr from-blue-800 from-10% to-custom-blue-5 text-white transition-all duration-900 hover:scale-150'>PDF</Button>
+                        <Button variant='contained' className=' content-end items-end fixed right-36 flex bg-paleta-2.3 text-white transition-transform duration-900 hover:scale-110 hover:bg-paleta-2.4'>PDF</Button>
+                        <Button variant='contained' className=' content-end items-end fixed right-8 flex bg-paleta-2.3 text-white transition-transform duration-900 hover:scale-110 hover:bg-paleta-2.4'>Assistir</Button>
                     </div>
-                    <div className="flex items-center justify-center rounded dark:bg-paleta-2.2 transition duration-200 ease-in-out transform hover:opacity-100 hover:scale-105 hover:z-50">
-                        <h5 className="px-10 max-w-sm text-2xl left-0 fixed text-white dark:text-white">
-                            Aula 4
+                    <div className="shadow-lg bg-black flex items-center justify-center rounded dark:bg-paleta-2.2 transition duration-200 ease-in-out transform hover:bg-paleta-2.2.1 hover:opacity-100 hover:z-50">
+                        <h5 className="px-10 max-w-sm text-2xl left-0 fixed text-black dark:text-black select-none">
+                            Aula #4
                         </h5>
-                        <div className='text-white/50 hover:text-white/90 p-2 max-w-sm items-center justify-center overflow-hidden text-s tracking-tight duration-1000 ease-in-out '>
+                        <div className='text-black p-2 max-w-xl items-center justify-center overflow-hidden text-s tracking-tight duration-1000 ease-in-out '>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad excepturi dolor consequuntur quae provident nihil minus error nemo, voluptatem velit deserunt sapiente mollitia officiis fuga facere illum quis! Non, tenetur!
                         </div>
-                        <Button variant='contained' className=' content-end items-end fixed right-36 flex bg-gradient-to-tr from-blue-800 from-10% to-custom-blue-5 text-white transition-all duration-900 hover:scale-150'>PDF</Button>
+                        <Button variant='contained' className=' content-end items-end fixed right-36 flex bg-paleta-2.3 text-white transition-transform duration-900 hover:scale-110 hover:bg-paleta-2.4'>PDF</Button>
+                        <Button variant='contained' className=' content-end items-end fixed right-8 flex bg-paleta-2.3 text-white transition-transform duration-900 hover:scale-110 hover:bg-paleta-2.4'>Assistir</Button>
                     </div>
-                    <div className="flex items-center justify-center rounded dark:bg-paleta-2.2 transition duration-200 ease-in-out transform hover:opacity-100 hover:scale-105 hover:z-50">
-                        <h5 className="px-10 max-w-sm text-2xl left-0 fixed text-white dark:text-white">
-                            Aula 5
+                    <div className="shadow-lg bg-black flex items-center justify-center rounded dark:bg-paleta-2.2 transition duration-200 ease-in-out transform hover:bg-paleta-2.2.1 hover:opacity-100 hover:z-50">
+                        <h5 className="px-10 max-w-sm text-2xl left-0 fixed text-black dark:text-black select-none">
+                            Aula #5
                         </h5>
-                        <div className='text-white/50 hover:text-white/90 p-2 max-w-sm items-center justify-center overflow-hidden text-s tracking-tight duration-1000 ease-in-out '>
+                        <div className='text-black p-2 max-w-xl items-center justify-center overflow-hidden text-s tracking-tight duration-1000 ease-in-out '>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad excepturi dolor consequuntur quae provident nihil minus error nemo, voluptatem velit deserunt sapiente mollitia officiis fuga facere illum quis! Non, tenetur!
                         </div>
-                        <Button variant='contained' className=' content-end items-end fixed right-36 flex bg-gradient-to-tr from-blue-800 from-10% to-custom-blue-5 text-white transition-all duration-900 hover:scale-150'>PDF</Button>
+                        <Button variant='contained' className=' content-end items-end fixed right-36 flex bg-paleta-2.3 text-white transition-transform duration-900 hover:scale-110 hover:bg-paleta-2.4'>PDF</Button>
+                        <Button variant='contained' className=' content-end items-end fixed right-8 flex bg-paleta-2.3 text-white transition-transform duration-900 hover:scale-110 hover:bg-paleta-2.4'>Assistir</Button>
                     </div>
-                    <div className="flex items-center justify-center rounded dark:bg-paleta-2.2 transition duration-200 ease-in-out transform hover:opacity-100 hover:scale-105 hover:z-50">
-                        <h5 className="px-10 max-w-sm text-2xl left-0 fixed text-white dark:text-white">
-                            Aula 6
+                    <div className="shadow-lg bg-black flex items-center justify-center rounded dark:bg-paleta-2.2 transition duration-200 ease-in-out transform hover:bg-paleta-2.2.1 hover:opacity-100 hover:z-50">
+                        <h5 className="px-10 max-w-sm text-2xl left-0 fixed text-black dark:text-black select-none">
+                            Aula #6
                         </h5>
-                        <div className='text-white/50 hover:text-white/90 p-2 max-w-sm items-center justify-center overflow-hidden text-s tracking-tight duration-1000 ease-in-out '>
+                        <div className='text-black p-2 max-w-xl items-center justify-center overflow-hidden text-s tracking-tight duration-1000 ease-in-out '>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad excepturi dolor consequuntur quae provident nihil minus error nemo, voluptatem velit deserunt sapiente mollitia officiis fuga facere illum quis! Non, tenetur!
                         </div>
-                        <Button variant='contained' className=' content-end items-end fixed right-36 flex bg-gradient-to-tr from-blue-800 from-10% to-custom-blue-5 text-white transition-all duration-900 hover:scale-150'>PDF</Button>
+                        <Button variant='contained' className=' content-end items-end fixed right-36 flex bg-paleta-2.3 text-white transition-transform duration-900 hover:scale-110 hover:bg-paleta-2.4'>PDF</Button>
+                        <Button variant='contained' className=' content-end items-end fixed right-8 flex bg-paleta-2.3 text-white transition-transform duration-900 hover:scale-110 hover:bg-paleta-2.4'>Assistir</Button>
                     </div>
-                    <div className="flex items-center justify-center rounded dark:bg-paleta-2.2 transition duration-200 ease-in-out transform hover:opacity-100 hover:scale-105 hover:z-50">
-                        <h5 className="px-10 max-w-sm text-2xl left-0 fixed text-white dark:text-white">
-                            Aula 7
+                    <div className="shadow-lg bg-black flex items-center justify-center rounded dark:bg-paleta-2.2 transition duration-200 ease-in-out transform hover:bg-paleta-2.2.1 hover:opacity-100 hover:z-50">
+                        <h5 className="px-10 max-w-sm text-2xl left-0 fixed text-black dark:text-black select-none">
+                            Aula #7
                         </h5>
-                        <div className='text-white/50 hover:text-white/90 p-2 max-w-sm items-center justify-center overflow-hidden text-s tracking-tight duration-1000 ease-in-out '>
+                        <div className='text-black p-2 max-w-xl items-center justify-center overflow-hidden text-s tracking-tight duration-1000 ease-in-out '>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad excepturi dolor consequuntur quae provident nihil minus error nemo, voluptatem velit deserunt sapiente mollitia officiis fuga facere illum quis! Non, tenetur!
                         </div>
-                        <Button variant='contained' className=' content-end items-end fixed right-36 flex bg-gradient-to-tr from-blue-800 from-10% to-custom-blue-5 text-white transition-all duration-900 hover:scale-150'>PDF</Button>
+                        <Button variant='contained' className=' content-end items-end fixed right-36 flex bg-paleta-2.3 text-white transition-transform duration-900 hover:scale-110 hover:bg-paleta-2.4'>PDF</Button>
+                        <Button variant='contained' className=' content-end items-end fixed right-8 flex bg-paleta-2.3 text-white transition-transform duration-900 hover:scale-110 hover:bg-paleta-2.4'>Assistir</Button>
                     </div>
                 </div>
             </div>
--- src/app/footer/Foot.tsx
+++ src/app/footer/Foot.tsx
@@ -2,16 +2,12 @@ export default function Foot(){
 
 return(
         <div>
-            <footer className="bg-black text-white z-50 justify-center items-center flex-col col-span-3 gap-3 p-4">
-            <ul>
-                <a href="#">Facebook</a>
+            <footer className="bg-black text-white relative z-50 justify-center items-center flex-row col-span-3 gap-3 p-4">
+            <ul className="flex flex-row justify-end gap-10 p-4">
+                <li className="transition-transform hover:scale-110"><a href="#">Facebook</a></li>
+                <li className="transition-transform hover:scale-110"><a href="#">Instagram</a></li>
+                <li className="transition-transform hover:scale-110"><a href="#">GitHub</a></li>
             </ul>            
-            <ul>
-                <a href="#">Facebook</a>
-            </ul>
-            <ul>
-                <a href="#">Facebook</a>
-            </ul>
             </footer>
         </div>
     );
